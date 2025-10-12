dd5b5a4c3fad687d25d5b0373724ffaf702aeafb
BatuhanGQskt <e231529@gmail.com>
2025-10-13 01:08:01 +0200

    Add usable files and Updated cargo.toml

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,10 @@
+mod parser;
+mod urls;
+mod errors;
+
+pub use parser::Parser;
+pub use urls::Urls;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use spacepix_lib::Parser;
+
+
 fn main() {
-    spacepix_lib::run()
+    let parser = Parser::default();
+    println!("{}", parser.apod_url());
+    println!("{}", parser.neows_url("2025-10-10"));
+    spacepix_lib::run();
 }
